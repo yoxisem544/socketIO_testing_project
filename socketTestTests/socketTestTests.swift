b53581a7f72b66869779e5ce44c590a3aee1490c
David <yoxisem544@gmail.com>
2016-01-06 11:51:40 +0800

    Initial Commit

---
+++ socketTestTests/socketTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  socketTestTests.swift
+//  socketTestTests
+//
+//  Created by David on 2016/1/6.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import XCTest
+@testable import socketTest
+
+class socketTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
